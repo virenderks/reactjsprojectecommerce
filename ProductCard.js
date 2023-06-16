fae1c235e43f4edc11d60257e3dcc0c1ed04567e
veer sharma <115632380+virenderks@users.noreply.github.com>
2023-06-16 18:29:33 +0530

    Add files via upload

--- ProductCard.js
+++ ProductCard.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Card, Button } from 'react-bootstrap';
+import { CartContext } from './CartContext';
 
 const ProductCard = ({ title, price, imageUrl }) => {
+  const { addToCart } = useContext(CartContext);
+
+  const handleAddToCart = () => {
+    addToCart({ title, price, imageUrl, quantity: 1 });
+  };
+
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={imageUrl} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>Price: ${price}</Card.Text>
-        <Button variant="primary">Add to Cart</Button>
+        <Button variant="primary" onClick={handleAddToCart}>
+          Add to Cart
+        </Button>
       </Card.Body>
     </Card>
   );
