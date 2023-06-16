bc1fe3336b30cfad1e4ad8d3b94d83253945cb32
veer sharma <115632380+virenderks@users.noreply.github.com>
2023-06-16 18:27:33 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import ProductCard from './ProductCard';
-import productsArr from './productsArr';
+import { CartContext } from './CartContext';
+import Cart from './Cart'; 
 
 const App = () => {
+  const [showCart, setShowCart] = useState(false);
+
+  const handleToggleCart = () => {
+    setShowCart(!showCart);
+  };
+
+  const cartElements = [
+    {
+      title: 'Colors',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+      quantity: 2,
+    },
+    {
+      title: 'Black and white Colors',
+      price: 50,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+      quantity: 3,
+    },
+    {
+      title: 'Yellow and Black Colors',
+      price: 70,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+      quantity: 1,
+    },
+  ];
+
   return (
-    <Container>
-      <Row>
-        {productsArr.map((product, index) => (
-          <Col key={index} md={4}>
-            <ProductCard {...product} />
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <CartContext.Provider value={{ cartElements }}>
+      <Container>
+        <Row>
+          {cartElements.map((product, index) => (
+            <Col key={index} md={4}>
+              <ProductCard {...product} />
+            </Col>
+          ))}
+        </Row>
+        <Button variant="primary" onClick={handleToggleCart}>
+          {showCart ? 'Close Cart' : 'Open Cart'}
+        </Button>
+        {showCart && <Cart />}
+      </Container>
+    </CartContext.Provider>
   );
 };
 
