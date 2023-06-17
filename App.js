8efdb347e309ed468727ab2c9a8184225e042637
veer sharma <115632380+virenderks@users.noreply.github.com>
2023-06-17 12:44:37 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,51 +1,61 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import ProductCard from './ProductCard';
-import Cart from './Cart';
-import CartContextProvider from './CartContext';
+ // ProductsScreen.js
+import React, { useEffect, useState } from 'react';
+import { Link, NavLink, Route, Switch } from 'react-router-dom';
+import HomeScreen from './HomeScreen';
+import Product from './Product';
 
-const App = () => {
-  const [showCart, setShowCart] = useState(false);
+const ProductsScreen = () => {
+  const [productsArr, setProductsArr] = useState([]);
 
-  const handleToggleCart = () => {
-    setShowCart(!showCart);
-  };
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('https://api.example.com/products');
+        const data = await response.json();
+        setProductsArr(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
 
-  const cartElements = [
-    {
-      title: 'Colors',
-      price: 100,
-      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
-    },
-    {
-      title: 'Black and white Colors',
-      price: 50,
-      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
-    },
-    {
-      title: 'Yellow and Black Colors',
-      price: 70,
-      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
-    },
-  ];
+    fetchProducts();
+  }, []);
 
   return (
-    <CartContextProvider>
-      <Container>
-        <Row>
-          {cartElements.map((product, index) => (
-            <Col key={index} md={4}>
-              <ProductCard {...product} />
-            </Col>
-          ))}
-        </Row>
-        <Button variant="primary" onClick={handleToggleCart}>
-          {showCart ? 'Close Cart' : 'Open Cart'}
-        </Button>
-        {showCart && <Cart />}
-      </Container>
-    </CartContextProvider>
+    <div>
+      <h1>The Generics</h1>
+      <nav>
+        <NavLink exact to="/" activeClassName="active">
+          Home
+        </NavLink>
+        <NavLink exact to="/products" activeClassName="active">
+          Products
+        </NavLink>
+        <NavLink exact to="/cart" activeClassName="active">
+          Cart
+        </NavLink>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={HomeScreen} />
+        <Route exact path="/products">
+          <h2>Music</h2>
+          <div>
+            {productsArr.map((product, index) => (
+              <Product
+                key={index}
+                title={product.title}
+                price={product.price}
+                imageUrl={product.imageUrl}
+              />
+            ))}
+          </div>
+        </Route>
+        <Route exact path="/cart">
+          {/* Cart component */}
+        </Route>
+      </Switch>
+    </div>
   );
 };
 
-export default App;
+export default ProductsScreen;
