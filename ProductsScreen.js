c45ddbfac5ae43723f1f8002926133a1b61b42be
veer sharma <115632380+virenderks@users.noreply.github.com>
2023-06-17 14:16:56 +0530

    Add files via upload

--- ProductsScreen.js
+++ ProductsScreen.js
@@ -1,98 +1,46 @@
  // ProductsScreen.js
 import React, { useEffect, useState } from 'react';
-import { Link, NavLink, Route, Switch } from 'react-router-dom';
-import HomeScreen from './HomeScreen';
-import Product from './Product';
-
-const fetchMovies = async () => {
-  const response = await fetch('https://swapi.dev/api/films');
-  const data = await response.json();
-  return data.results;
-};
 
 const ProductsScreen = () => {
   const [movies, setMovies] = useState([]);
-  const [showForm, setShowForm] = useState(false);
-  const [newMovieObj, setNewMovieObj] = useState({ title: '', price: '', imageUrl: '' });
+  const [loading, setLoading] = useState(true);
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    setMovies([...movies, newMovieObj]);
-    setShowForm(false);
-    setNewMovieObj({ title: '', price: '', imageUrl: '' });
+  const fetchMovies = async () => {
+    const response = await fetch('/api/movies');
+    const data = await response.json();
+    return data;
   };
 
   useEffect(() => {
     const fetchMoviesData = async () => {
       const moviesData = await fetchMovies();
       setMovies(moviesData);
+      setLoading(false);
     };
     fetchMoviesData();
   }, []);
 
+  const handleDeleteMovie = async (id) => {
+    await fetch(`/api/movies/${id}`, { method: 'DELETE' });
+    setMovies(movies.filter((movie) => movie.id !== id));
+  };
+
   return (
     <div>
-      <h1>The Generics</h1>
-      <nav>
-        <NavLink exact to="/" activeClassName="active">
-          Home
-        </NavLink>
-        <NavLink exact to="/products" activeClassName="active">
-          Products
-        </NavLink>
-        <NavLink exact to="/cart" activeClassName="active">
-          Cart
-        </NavLink>
-      </nav>
-      <Switch>
-        <Route exact path="/" component={HomeScreen} />
-        <Route exact path="/products">
-          <h2>Music</h2>
-          {showForm && (
-            <form onSubmit={handleFormSubmit}>
-              <label htmlFor="title">Title:</label>
-              <input
-                type="text"
-                id="title"
-                value={newMovieObj.title}
-                onChange={(e) => setNewMovieObj({ ...newMovieObj, title: e.target.value })}
-                required
-              />
-              <label htmlFor="price">Price:</label>
-              <input
-                type="number"
-                id="price"
-                value={newMovieObj.price}
-                onChange={(e) => setNewMovieObj({ ...newMovieObj, price: e.target.value })}
-                required
-              />
-              <label htmlFor="imageUrl">Image URL:</label>
-              <input
-                type="text"
-                id="imageUrl"
-                value={newMovieObj.imageUrl}
-                onChange={(e) => setNewMovieObj({ ...newMovieObj, imageUrl: e.target.value })}
-                required
-              />
-              <button type="submit">Add Movie</button>
-            </form>
-          )}
-          <button onClick={() => setShowForm(!showForm)}>Add Movies</button>
-          <div>
-            {movies.map((movie, index) => (
-              <Product
-                key={index}
-                title={movie.title}
-                price={movie.price}
-                imageUrl={movie.imageUrl}
-              />
-            ))}
-          </div>
-        </Route>
-        <Route exact path="/cart">
-          {/* Cart component */}
-        </Route>
-      </Switch>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <h3>{movie.title}</h3>
+              <p>Price: ${movie.price}</p>
+              <img src={movie.imageUrl} alt={movie.title} />
+              <button onClick={() => handleDeleteMovie(movie.id)}>Delete Movie</button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
