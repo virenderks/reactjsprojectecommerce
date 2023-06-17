ba2804fd7d830f225d292b8b8141b92b0e4b772f
veer sharma <115632380+virenderks@users.noreply.github.com>
2023-06-17 19:13:03 +0530

    Add files via upload

--- LoginScreen.js
+++ LoginScreen.js
@@ -1,34 +1,21 @@
-// LoginScreen.js
-import React, { useState } from 'react';
+ // LoginScreen.js
+import React, { useContext, useState } from 'react';
+import { AuthContext } from './AuthContext';
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { login } = useContext(AuthContext);
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email,
-          password,
-          returnSecureToken: true,
-        }),
-      });
-      const data = await response.json();
+      // Make the login request and obtain the token
+      const token = 'yourTokenValue';
 
-      if (response.ok) {
-        console.log('idToken:', data.idToken);
-        // Do something with the idToken, such as storing it in local storage or global state
+      // Store the token in the context
+      login(token);
 
-        // Redirect to another page or update UI as needed
-      } else {
-        console.error('Authentication failed:', data.error.message);
-        // Show an alert or update UI to inform the user about authentication failure
-      }
+      // Redirect to another page or update UI as needed
     } catch (error) {
       console.error('Error logging in:', error);
     }
@@ -36,15 +23,7 @@ const LoginScreen = () => {
 
   return (
     <div>
-      <h2>Login</h2>
-      <div>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </div>
+      {/* Your login form JSX */}
       <button onClick={handleLogin}>Login</button>
     </div>
   );
